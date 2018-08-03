c6aa3976c1e6f085d43a13151d6ebfd28c207e6e
tomkaos <konyatamas01@gmail.com>
2018-08-03 20:54:37 +0200

    Modify hosts

--- lakodex_api/settings/production.py
+++ lakodex_api/settings/production.py
@@ -3,6 +3,8 @@ from lakodex_api.settings.common import *
 
 DEBUG = False
 
+ALLOWED_HOSTS = ['intense-citadel-86043.herokuapp.com']
+
 CORS_ORIGIN_ALLOW_ALL = True
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
